324de23ec524c75a13af5ed8b58a3ef2b08072e3
wertypotom <werty.potom@gmail.com>
2021-08-03 16:41:11 +0600

    initial commit

---
+++ src/Submenu.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaColumns } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
+const Submenu = () => {
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+
+  const container = useRef(null);
+  const [columns, setColums] = useState('col-2');
+
+  useEffect(() => {
+    setColums('col-2');
+    const submenu = container.current;
+    const { center, bottom } = location;
+
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+
+    if (links.length === 3) {
+      setColums('col-3');
+    }
+    if (links.length > 3) {
+      setColums('col-4');
+    }
+  }, [location, links]);
+
+  return (
+    <aside
+      className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={index}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
